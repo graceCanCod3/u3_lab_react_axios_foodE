8dde9e5ffb79d1206263463af2faf3a679ee6b95
GideonCove <gideoncove@gmail.com>
2024-06-20 13:37:09 -0400

    test

---
+++ u3_lab_react_axios_foodE/src/components/components.details/StarterDetails.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import axios from 'axios'
+import { BASE_URL } from '../../globals'
+
+
+const StarterDetails = ({ countries }) => {
+    let { mealId } = useParams()
+    const [meal, setMeal] = useState([])
+
+    useEffect(() => {
+        const getStarterMeal = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}lookup.php?i=${mealId}`)
+                setMeal(response.data.meals)
+            } catch (error) {
+                console.error("Error fetching meals: ", error)
+            }
+        }
+        getStarterMeal()
+    }, [mealId])
+
+    let navigate = useNavigate()
+
+    const showMeal = (mealId) => {
+        navigate(`/Starter/meal/${mealId}`)
+    }
+
+
+    return (
+        <div>
+            {meal && meal.length > 0 ? (
+                <div className="meals-list">
+                    {meal.map((meal) => (
+                        <div onClick={()=> showMeal(meal.idMeal)}key={meal.idMeal} className="meal-item">
+                            <h3>{meal.strMeal}</h3>
+                            <img src={meal.strMealThumb} alt={meal.strMeal} />
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No meals found for.</p>
+            )}
+        </div>
+    )
+}
+
+export default StarterDetails
